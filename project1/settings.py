64480235aef6251f153ab394c1524978b659b8d6
sripad-kulkarni <sripad.kulkarni@salesforce.com>
2019-08-28 17:42:34 +0530

    sdasd

--- project1/settings.py
+++ project1/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
