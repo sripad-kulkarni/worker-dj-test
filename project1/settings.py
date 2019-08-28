219f8873966955e6cbb980edfa60c7cc66fce959
sripad-kulkarni <sripad.kulkarni@salesforce.com>
2019-08-28 17:44:12 +0530

    sdasd

--- project1/settings.py
+++ project1/settings.py
@@ -13,15 +13,15 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
-WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'root') 
+WHITENOISE_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles', 'root') 
 
 
 # Quick-start development settings - unsuitable for production
