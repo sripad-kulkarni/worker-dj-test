fafe7f819c1bef930ecea1ebbecf9e3f3d4abb37
sripad-kulkarni <sripad.kulkarni@salesforce.com>
2019-08-28 16:52:56 +0530

    sdasd

--- project1/wsgi.py
+++ project1/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 from whitenoise.django import DjangoWhiteNoise
 
