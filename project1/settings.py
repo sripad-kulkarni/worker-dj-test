f79642ef2a7551fc451d30f838ce9c355a438497
sripad-kulkarni <sripad.kulkarni@salesforce.com>
2019-08-28 18:12:56 +0530

    sdasd

--- project1/settings.py
+++ project1/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-        		os.path.join(PROJECT_ROOT, "templates")
+        		os.path.join(PROJECT_ROOT, "static")
         ],
         'APP_DIRS': True,
         'OPTIONS': {
